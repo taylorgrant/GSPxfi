41c9bdc7d05d61cfad2d90b14f6dc42a04b2942e
taylorgrant <taylorgrant38@gmail.com>
2024-08-20 10:05:48 -0700

    pulling master matrix ids and links

---
+++ R/get_matrix_ids.R
@@ -0,0 +1,59 @@
+#' Read in the Xfinity Social Matrices list
+#'
+#' The Xfinity social matrices list is the overall sheet that contains hyperlinks to each
+#' social matrix. The social matrices cover all social channels for each LOB of Xfinity.
+#'
+#'
+#' @param sheet The name or number of the sheet within the workbook
+#' @param full_range The lower range that the sheet covers (e.g., B43)
+#' @param email A google email to authenticate with \code{"\link[googlesheets4]{googlesheets4-package}}
+#'
+#' @return Dataframe with the LOB, the matrix/social platform name, and the link associated with each
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' q3_matrices <- get_matrix_ids(sheet = 3, full_range = "B43", email = first_last.email.com)
+#' }
+get_matrix_ids <- function(sheet, full_range, email) {
+  options(gargle_oauth_email = email)
+  # master ID
+  id <- "1WF9GiVxEB5whILVtvbAxB5nggviR32gSJxq2L0h3--4"
+  ss <- googlesheets4::gs4_get(id)
+  # read in text format
+  list_text <- googlesheets4::read_sheet(ss, sheet = sheet) |>
+    janitor::clean_names() |>
+    dplyr::select(lob, q324_matrix) |>
+    tidyr::fill(lob, .direction = "down")
+  # read in hyperlinks
+  cells <- googlesheets4::range_read_cells(ss, sheet = sheet, range = glue::glue("A1:{full_range}"), cell_data = "full")
+  # function to extract hyperlinks
+  extract_hyperlink <- function(cell) {
+    if (!is.null(cell$hyperlink)) {
+      return(cell$hyperlink)
+    } else {
+      return(NA)
+    }
+  }
+  # use function
+  hyperlinks <- cells %>%
+    dplyr::mutate(hyperlink = purrr::map_chr(cell, extract_hyperlink)) |>
+    dplyr::filter(str_detect(loc, "^B")) |>
+    dplyr::filter(row != 1) |>
+    dplyr::select(hyperlink)
+  # bind together
+  out <- cbind(list_text, hyperlinks)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
