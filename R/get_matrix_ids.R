91ad617832d9f4f88d35600ba731ea55debdd6af
taylorgrant <taylorgrant38@gmail.com>
2024-09-09 09:38:29 -0700

    pulling matrix ids from master sheet

--- R/get_matrix_ids.R
+++ R/get_matrix_ids.R
@@ -3,6 +3,8 @@
 #' The Xfinity social matrices list is the overall sheet that contains hyperlinks to each
 #' social matrix. The social matrices cover all social channels for each LOB of Xfinity.
 #'
+#' Note that the user is required to have the workbook ID of the sheet saved as an environmental variable `XFI_MATRIX_ID`
+#'
 #'
 #' @param sheet The name or number of the sheet within the workbook
 #' @param full_range The lower range that the sheet covers (e.g., B43)
@@ -18,7 +20,7 @@
 get_matrix_ids <- function(sheet, full_range, email) {
   options(gargle_oauth_email = email)
   # master ID
-  id <- "1WF9GiVxEB5whILVtvbAxB5nggviR32gSJxq2L0h3--4"
+  id <- Sys.getenv("XFI_MATRIX_ID")
   ss <- googlesheets4::gs4_get(id)
   # read in text format
   list_text <- googlesheets4::read_sheet(ss, sheet = sheet) |>
