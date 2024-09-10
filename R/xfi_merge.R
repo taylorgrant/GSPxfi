18a3ecaa7b26b4d2e1f397d713277cceeb544a9b
taylorgrant <taylorgrant38@gmail.com>
2024-09-10 15:45:28 -0700

    pinterest doesn't use ad accounts?!?!

--- R/xfi_merge.R
+++ R/xfi_merge.R
@@ -17,19 +17,75 @@
 #' }
 xfi_merge <- function(lob){
   # filter down to specific LOB
-  tmp_sprinklr <- sprinklr_data |>
-    dplyr::filter(stringr::str_detect(ad_account, lob)) |>
-    apply_nomenclature()
+
+  # pinterest only uses a single ad account, so must use the client_code or mmm_category
+  if (any(stringr::str_detect(names(sprinklr_data), "pinterest|reddit"))) {
+    tmp_sprinklr <- sprinklr_data |>
+      apply_nomenclature() |>
+      dplyr::filter(dplyr::case_when(lob == "Central" ~ c_client_code == "Comcast Central",
+                                     lob == "NED" ~ c_client_code == "Comcast North",
+                                     lob == "West" ~ c_client_code == "Comcast West",
+                                     lob == "Xfinity Mobile" ~ c_mmm_category == "Xfinity Mobile",
+                                     lob == "Retargeting" ~ c_mmm_category == "Retargeting"))
+  } else {
+    # otherwise use the ad account
+    tmp_sprinklr <- sprinklr_data |>
+      dplyr::filter(stringr::str_detect(ad_account, lob)) |>
+      apply_nomenclature()
+  }
+
 
   # fuzzy full join sprinklr data to matrix data
-  tmp_full <- tmp_sprinklr |>
-    fuzzyjoin::fuzzy_full_join(tidyr::unnest(matrix_data, col = matrix_data),
-                               by = c("ad_variant_name" = "placement_name"),
-                               match_fun = stringr::str_detect) |>
-    clean_headers() |>
-    dplyr::filter(!is.na(campaign_name)) |>
-    dplyr::distinct(part2, ad_post_permalink, platform_position, ad_objective,
-                    total_results, spend, impressions, .keep_all = TRUE)
+  if (any(stringr::str_detect(names(tmp_sprinklr), "facebook"))) {
+
+    tmp_full <- tmp_sprinklr |>
+      fuzzyjoin::fuzzy_full_join(tidyr::unnest(matrix_data, col = matrix_data),
+                                 by = c("ad_variant_name" = "placement_name"),
+                                 match_fun = stringr::str_detect) |>
+      clean_headers() |>
+      dplyr::filter(!is.na(campaign_name)) |>
+      dplyr::distinct(part2, ad_post_permalink, platform_position, ad_objective,
+                      total_results, spend, impressions, .keep_all = TRUE)
+
+  } else if (any(stringr::str_detect(names(tmp_sprinklr), "snapchat"))) {
+
+    tmp_full <- tmp_sprinklr |>
+      fuzzyjoin::fuzzy_full_join(tidyr::unnest(matrix_data, col = matrix_data),
+                                 by = c("ad_variant_name" = "placement_name"),
+                                 match_fun = stringr::str_detect) |>
+      clean_headers() |>
+      dplyr::filter(!is.na(campaign_name)) |>
+      dplyr::distinct(part2, ad_objective, total_results, spend, impressions, .keep_all = TRUE)
+
+  } else if (any(stringr::str_detect(names(tmp_sprinklr), "pinterest"))) {
+
+    tmp_full <- tmp_sprinklr |>
+      fuzzyjoin::fuzzy_full_join(tidyr::unnest(matrix_data, col = matrix_data),
+                                 by = c("ad_variant_name" = "placement_name"),
+                                 match_fun = stringr::str_detect) |>
+      clean_headers() |>
+      dplyr::filter(!is.na(campaign_name)) |>
+      dplyr::distinct(part2, ad_post_permalink, ad_objective, total_results,
+                      spend, impressions, .keep_all = TRUE)
+
+  } else if (any(stringr::str_detect(names(tmp_sprinklr), "reddit"))) {
+    tmp_full <- tmp_sprinklr |>
+      fuzzyjoin::fuzzy_full_join(tidyr::unnest(matrix_data, col = matrix_data),
+                                 by = c("ad_variant_name" = "placement_name"),
+                                 match_fun = stringr::str_detect) |>
+      clean_headers() |>
+      dplyr::filter(!is.na(campaign_name))
+
+  } else if (any(stringr::str_detect(names(tmp_sprinklr), "tik_tok"))) {
+    tmp_full <- tmp_sprinklr |>
+      fuzzyjoin::fuzzy_full_join(tidyr::unnest(matrix_data, col = matrix_data),
+                                 by = c("ad_variant_name" = "placement_name"),
+                                 match_fun = stringr::str_detect) |>
+      clean_headers() |>
+      dplyr::filter(!is.na(campaign_name))
+  }
+
+
 
   # report out on where we're missing
   report1 <- tmp_full |>
