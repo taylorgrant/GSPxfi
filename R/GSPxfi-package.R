7b1287d7e5b738947a9362c17c58176df91bce05
taylorgrant <taylorgrant38@gmail.com>
2024-11-12 18:48:27 -0800

    package

---
+++ R/GSPxfi-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats AIC
+#' @importFrom stats as.formula
+#' @importFrom stats coef
+#' @importFrom stats lm
+#' @importFrom stats predict
+## usethis namespace: end
+NULL
